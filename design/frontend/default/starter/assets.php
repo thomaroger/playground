15696c836217ff0ef1a567a91dd8251947d85e12
nicolaslabbe <nicolas.labbe@adfab.fr>
2014-09-04 18:20:40 +0200

    add respondjs into asset.php

--- design/frontend/default/starter/assets.php
+++ design/frontend/default/starter/assets.php
@@ -6,32 +6,42 @@ return array(
 				'root_path' => array(
 					__DIR__ . '/../../../../design/frontend/default/starter/assets',
 				),
-			    'collections' => array(
-                    'frontend_images' => array(
-                        'assets' => array(
-                            'images/**/*.png',
-                            'images/**/*.jpg',
-                        ),
-                        'options' => array(
-                            'move_raw' => true,
-                            'output' => 'frontend',
-                        )
-                    ),
-	                'frontend_css' => array(
-	                    'assets' => array(
-	                        'css/starter.css'
-	                    ),
-	                    'options' => array(
-	                        'output' => 'frontend/css/starter'
-	                    )
-	                ),
-                ),
+		    'collections' => array(
+					'frontend_images' => array(
+						'assets' => array(
+							'images/**/*.png',
+							'images/**/*.jpg',
+						),
+						'options' => array(
+							'move_raw' => true,
+							'output' => 'frontend',
+						)
+					),
+					'frontend_css' => array(
+						'assets' => array(
+							'css/starter.css'
+						),
+						'options' => array(
+							'output' => 'frontend/css/starter'
+						)
+					),
+
+					'frontend_js' => array(
+						'assets' => array(
+							'js/respond.src.js'
+						),
+						'options' => array(
+							'output' => 'frontend/js/frontend_js'
+						)
+					)
+        ),
 			),
 		),
-        'routes' => array(
-            'frontend.*' => array(
-                '@frontend_css' => '@frontend_css',
-            ),
-        ),
+    'routes' => array(
+      'frontend.*' => array(
+        '@frontend_css' => '@frontend_css',
+        '@frontend_js' => '@frontend_js',
+      ),
+    ),
 	),
 );
