40273928c49daed808a8c7040d8b4c2f9cbccc3f
GrG <gregory.besson@adfab.fr>
2014-09-15 20:33:45 +0200

    assets.php update

--- design/frontend/default/starter/assets.php
+++ design/frontend/default/starter/assets.php
@@ -1,47 +1,52 @@
 <?php
 return array(
-	'assetic_configuration' => array(
-		'modules' => array(
-			'default_base' => array(
-				'root_path' => array(
-					__DIR__ . '/../../../../design/frontend/default/starter/assets',
-				),
-		    'collections' => array(
-					'frontend_images' => array(
-						'assets' => array(
-							'images/**/*.png',
-							'images/**/*.jpg',
-						),
-						'options' => array(
-							'move_raw' => true,
-							'output' => 'frontend',
-						)
-					),
-					'frontend_css' => array(
-						'assets' => array(
-							'css/starter.css'
-						),
-						'options' => array(
-							'output' => 'frontend/css/starter'
-						)
-					),
-
-					'frontend_js' => array(
-						'assets' => array(
-							'js/respond.src.js'
-						),
-						'options' => array(
-							'output' => 'frontend/js/frontend_js'
-						)
-					)
+    'assetic_configuration' => array(
+        'modules' => array(
+            'frontend' => array(
+                'collections' => array(
+                    'frontend_images' => null,
+                    'frontend_css' => array(
+                        'assets' => array(
+                            'styles.css' => __DIR__ . '/../../../../design/frontend/default/starter/assets/css/starter.css',
+                            'ie7.css' => null,
+                            'ie7.css' => null,
+                            'ie.css' => null
+                        ),
+                        'filters' => array(),
+                        'options' => array(
+                            'output' => 'frontend/css/main'
+                        ),
+                    ),
+                    'head_frontend_js' => array(
+                        'assets' => array(
+                            'respond.js'     => __DIR__ . '/../../../../design/frontend/default/starter/assets/js/respond.src.js',
+                        ),
+                    ),
+                )  
+            ),
+            'default_starter' => array(
+                'root_path' => array(
+                    __DIR__ . '/../../../../design/frontend/default/starter/assets'
+                ),
+                'collections' => array(
+                    'starter_images' => array(
+                        'assets' => array(
+                            'images/**/*.png',
+                            'images/**/*.jpg'
+                        ),
+                        'options' => array(
+                            'move_raw' => true,
+                            'output' => 'frontend'
+                        )
+                    )
+                )
+            )
         ),
-			),
-		),
-    'routes' => array(
-      'frontend.*' => array(
-        '@frontend_css' => '@frontend_css',
-        '@frontend_js' => '@frontend_js',
-      ),
-    ),
-	),
+        'routes' => array(
+            'frontend.*' => array(
+                //'@frontend_css' => '@starter_css',
+                //'@head_frontend_js' => '@starter_js'
+            )
+        )
+    )
 );
